06a6690142633d705a52c5c2b4d51a1f4f4eaed2
sunyata2022 <sunyata2022@hotmail.com>
2023-06-25 15:13:33 +0800

    export sm4 & sm5

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import sm2 from './sm2';
-import sm4 from './sm4';
+import sm4, { getInterval as getSM4Interval } from './sm4';
+import sm5, { getInterval as getSM5Interval } from './sm5';
 
-export {
-    sm2,
-    sm4,
-}
+export { sm2, sm4, getSM4Interval, sm5, getSM5Interval };
