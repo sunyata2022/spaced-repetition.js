f56aec78e7d4f2b0bc83bcb1133b68ed2c51c4a2
sunyata2022 <sunyata2022@hotmail.com>
2023-06-30 08:29:31 +0800

    fix bug:next count match next interval

--- src/sm2.ts
+++ src/sm2.ts
@@ -12,13 +12,6 @@ export interface SM2Result {
   needRepeat: boolean;
 }
 
-/**
- * Calculates the interval based on the count and efactor.
- *
- * @param {number} count - The count parameter.
- * @param {number} efactor - The efactor parameter.
- * @return {number} The calculated interval.
- */
 function calculateInterval(count: number, efactor: number): number {
   if (count === 1) return 1;
   if (count === 2) return 6;
@@ -26,13 +19,6 @@ function calculateInterval(count: number, efactor: number): number {
   return Math.round(Math.pow(efactor, count - 2) * 6);
 }
 
-/**
- * Calculates the next interval and other values for a given SM2Item, based on its
- * current values. If the quality is less than 4, the item is marked for repetition.
- *
- * @param {SM2Item} item - the SM2Item to calculate the next interval for
- * @return {SM2Result} an object with the next SM2Item and a boolean indicating if the item needs to be repeated
- */
 function sm2(item: SM2Item): SM2Result {
   let { count = 0, efactor = 2.5, quality } = item;
 
@@ -45,7 +31,7 @@ function sm2(item: SM2Item): SM2Result {
     nextItem.count = 0;
   } else {
     nextItem.count = count + 1;
-    nextItem.interval = calculateInterval(count, efactor);
+    nextItem.interval = calculateInterval(nextItem.count, efactor);
   }
 
   return { item: nextItem, needRepeat };
